8bf25efd45d7eb55ccf2e9aa5a6ad67cc0d22011
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-07-10 09:39:20 +0530

    configcommit

---
+++ orderconfig.js
@@ -0,0 +1,13 @@
+const config = {
+    user :'sa',
+    password :'sa_123',
+    server:'DESKTOP-4CSS7FV',
+    database:'ordersdb',
+    port : 1433,
+    options: {
+        encrypt: false,
+        trustServerCertificate: true
+    }
+}
+
+module.exports = config; 
