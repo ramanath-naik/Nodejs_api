ecb6535b26f77d5f9f2364070e15b45194a3aeec
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-07-11 09:21:19 +0530

    classcommit

---
+++ Team_members.js
@@ -0,0 +1,14 @@
+class Team_members{
+    constructor(member_id,member_name,member_dob,member_city){
+        member_id=id;
+        member_name=Name;
+        member_dob=dob;
+        member_city=city;
+
+    }
+
+}
+
+
+module.exports = Team_members;
+
